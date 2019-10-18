3b21757777133d7f8a746ac6b2076f7441e72e6a
Viktor Vyshnevskyi <viktor.vyshnevsky@captify.co.uk>
2019-10-18 17:49:28 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const accessCamera = () => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image');
+    input.setAttribute('capture', 'camera');
+    input.click();
+  };
+  
+  // const accessCamera = () => {
+  //   Navigator.getUserMedia()
+  // };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {accessCamera()}
     </div>
   );
 }
